196f624fc1390c7bdf429f6816b8e8cce893daa7
Rim Zabarov <rim@zabarov.com>
2026-04-30 03:18:09 +0300

    Fill Docara UI translations

--- docara/source/docs/en/.lang.php
+++ docara/source/docs/en/.lang.php
@@ -1,5 +1,18 @@
 <?php
 return [
+    'actions' => 'Actions',
+    'dark' => 'Dark mode',
+    'default' => 'Default',
     'search' => 'Search',
+    'notFound' => 'Nothing found',
+    'settings' => 'Settings',
+    'on' => 'On',
+    'off' => 'Off',
     'edit article' => 'Edit article',
+    'report a bug' => 'Report a bug',
+    'wide' => 'Wide layout',
+    'text size' => 'Text size',
+    'reduced' => 'Reduced',
+    'increased' => 'Increased',
+    'navigation' => 'Navigation',
 ];
